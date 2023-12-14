d403deba96bb3ae4558edec1eb6f04fc17788a9f
NISHAN75 <nishandas880@gmail.com>
2023-12-15 01:33:10 +0600

    testing

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+
+TweenMax.staggerFrom(".logo" , 1.6 , {
+	delay: 0.2,
+	opacity:0,
+	y:20,
+	ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom(".logo" , 1.6 , {
+	delay: 0.2,
+	opacity:0,
+	y:20,
+	ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom(".search" , 1.6 , {
+	delay: 0.2,
+	opacity:0,
+	y:20,
+	ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom(".img" , 3 , {
+	delay: 0.7,
+	opacity:0,
+	y:20,
+	ease: Expo.easeInOut
+});
+TweenMax.staggerFrom(".bottom-bar" , 2 , {
+	delay: 2,
+	opacity:0,
+	x: -40,
+	ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom("i" , 2 , {
+	delay: 2.5,
+	opacity:0,
+	y:40,
+	ease: Expo.easeInOut
+});
+TweenMax.staggerFrom(".title span" , 3 , {
+	delay: 3,
+	opacity:0,
+	x:-100,
+	ease: Expo.easeInOut
+});
+
+
